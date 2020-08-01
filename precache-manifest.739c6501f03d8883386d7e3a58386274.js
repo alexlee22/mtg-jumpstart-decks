092eb7872b575fe29f674a2a5c3fbb477e73bac4
Alex Lee <10814755+alexlee22@users.noreply.github.com>
2020-08-02 02:52:51 +1000

    Updates

---
+++ precache-manifest.739c6501f03d8883386d7e3a58386274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88ec24cb158f17472a4e34a123ef690",
+    "url": "/mtg-jumpstart-decks/index.html"
+  },
+  {
+    "revision": "b42e73e44057a08875e1",
+    "url": "/mtg-jumpstart-decks/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "69131554c1192fa6e87b",
+    "url": "/mtg-jumpstart-decks/static/js/2.497d7a1b.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/mtg-jumpstart-decks/static/js/2.497d7a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42e73e44057a08875e1",
+    "url": "/mtg-jumpstart-decks/static/js/main.ddc2c32f.chunk.js"
+  },
+  {
+    "revision": "1670676b8b1852b16927",
+    "url": "/mtg-jumpstart-decks/static/js/runtime-main.b2adc788.js"
+  }
+]);
