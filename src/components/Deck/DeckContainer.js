4c633c87a446ab64af47473204237fbaeb7cf698
Alex Lee <10814755+alexlee22@users.noreply.github.com>
2020-08-10 22:24:41 +1000

    Update colors to gradients
    :blobparty:

--- src/components/Deck/DeckContainer.js
+++ src/components/Deck/DeckContainer.js
@@ -4,15 +4,15 @@ import DeckContents from './DeckContents.js'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
 
 const mana_colors = {
-  'w': '#ffeb3b',
-  'u': '#3f51b5',
-  'b': '#673ab7',
-  'r': '#f44336',
-  'g': '#4caf50',
-  'm': '#e91e63'
+  'w': 'linear-gradient(62deg, #e65c00 20%, #F9D423 125%)',
+  'u': 'linear-gradient(135deg, #1A2980 20%, #26D0CE 125%)',
+  'b': 'linear-gradient(135deg, #3c1053 20%, #ad5389 125%)',
+  'r': 'linear-gradient(135deg, #BD3F32 20%, #CB356B 125%)',
+  'g': 'linear-gradient(132deg, #16A085 20%, #F4D03F 125%)',
+  'm': 'linear-gradient(135deg, #654ea3 20%, #C850C0 66%, #FFCC70 125%)'
 }
 
 const useStyles = makeStyles(theme => ({
@@ -21,14 +21,24 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     color: 'white'
   },
+  headingContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '20px',
+    
+  },
   card: {
-    margin: '20px'
+    margin: '20px',
+    border: '0px solid black',
+    borderRadus: '5px'
   },
   heading: {
-    textTransform: 'capitalize'
+    textTransform: 'capitalize',
+    color: 'hsla(0, 20%, 125%, 0.92)'
   },
   cardContent: {
-    padding: 0
+    borderTop: "0px solid black",
+    padding: '20px'
   }
 }));
 
@@ -41,16 +51,17 @@ export default function DeckContainer(props) {
   return (
     <Card className={classes.card} style={searchResults.indexOf(props.id) > -1 ? {} : {display: 'none'}}>
       
-        <div style={{display: 'flex', justifyContent: 'space-between', padding: '20px', backgroundColor: mana_colors[data.color[0]]}} onClick={() => setIsOpen(!isOpen)}>
+        <div 
+          className={classes.headingContainer} 
+          style={ data.color.length > 1 ? { backgroundImage:mana_colors['m'] } : { backgroundImage: mana_colors[data.color[0]]}} 
+          onClick={() => setIsOpen(!isOpen)}
+        >
           <Typography variant="h6" component="h6" className={classes.heading}>
             {data.name}
           </Typography>
-          <FiberManualRecordIcon 
-            fontSize="large" 
-            style={ data.color.length > 1 ? {color:mana_colors['m']} : {color:mana_colors[data.color[0]]} } 
-          />
+          
         </div>
-        <div style={isOpen ? {padding: '20px'} : {display: 'none'}}>
+        <div className={classes.cardContent} style={isOpen ? {} : {display: 'none'}}>
           <DeckContents deck={data.deck} />
         </div>
       
